3d8a5deaf71fc225461e2f00f884fffef818effb
leflu <salinasnicolas105@gmail.com>
2023-10-17 04:45:48 -0300

    barra de busqueda, reviews y sass

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const peliculas = [
     {
-      nombre: "Interestellar",
+      nombre: "Interstellar",
       director: "Christopher Nolan",
       sinopsis: "Un grupo de exploradores viaja a través de un agujero de gusano en busca de un nuevo hogar para la humanidad."
     },
@@ -25,14 +25,7 @@ const peliculas = [
       sinopsis: "Un taxista solitario y alienado se convierte en un justiciero en las peligrosas calles de Nueva York."
     }
 ];
-
-for(let i = 0; i < peliculas.length; i++){
-  const btn = document.querySelector(`#info-btn${i}`);
-  btn.addEventListener("click", function(){
-    mostrarInfo(i);
-  })
-}
-function mostrarInfo(indice){
+/* function mostrarInfo(indice){
   const tituloPelicula = document.querySelector("#titulo-pelicula");
   const directorPelicula = document.querySelector("#director-pelicula");
   const sinopsisPelicula = document.querySelector("#sinopsis-pelicula");
@@ -40,6 +33,11 @@ function mostrarInfo(indice){
   directorPelicula.textContent = "Dirigidar por: " + peliculas[indice].director;
   sinopsisPelicula.textContent = peliculas[indice].sinopsis;
 }
+for(let i = 0; i < peliculas.length; i++){
+  const btn = document.querySelector(`#info-btn${i}`);
+  btn.addEventListener("click",function(){
+    mostrarInfo(i)})
+} */
 
 function buscarPelicula() {
   const barraBusqueda = document.getElementById("movie-searchbar").value.toLowerCase();
@@ -69,13 +67,86 @@ function buscarPelicula() {
 }
 
 const botonBusqueda = document.getElementById("movie-searchbtn")
+botonBusqueda.addEventListener("click", buscarPelicula)
 
-botonBusqueda.addEventListener("click", function(){
-  buscarPelicula();
+
+const resenasUsuarios = document.querySelector("#container-resenas")
+const formulario = document.querySelector("#resena-form")
+
+let resenas = []
+
+function errorResena(msg){
+  const mensajeError = document.querySelector("#resena-titulo")
+  mensajeError.textContent = msg;
+
+  setTimeout(()=>{
+    mensajeError.remove()
+  },3000)
+}
+
+function limpiarResenas(){
+  while(resenasUsuarios.firstChild){
+    resenasUsuarios.removeChild(resenasUsuarios.firstChild)
+  }
+}
+function publicarResena(){
+  limpiarResenas();
+  resenas.forEach(resena =>{
+    const tituloResena = document.createElement("h2");
+    const ratingText = document.createElement("h3");
+    const resenaText = document.createElement("p");
+    const hr = document.createElement("hr")
+    hr.setAttribute("class", "hr-resena")
+
+    tituloResena.textContent = resena.nombre;
+    ratingText.textContent = "Puntuacion: " + resena.rating;
+    resenaText.textContent = "Reseña: " + resena.resena;
+    
+    resenasUsuarios.appendChild(tituloResena)
+    resenasUsuarios.appendChild(ratingText)
+    resenasUsuarios.appendChild(resenaText)
+    resenasUsuarios.appendChild(hr)
+  })
+
+  guardarStorage();
+}
+
+
+
+
+function agregarResena(evt){
+  evt.preventDefault();
+  const nombrePelicula = document.querySelector("#input-movie_name").value;
+  const ratingPelicula = document.querySelector("#input-rating").value;
+  const resenaPelicula = document.querySelector("#input-resena").value;
+
+  if(nombrePelicula === '' || resenaPelicula === '' || ratingPelicula === ''){
+    errorResena("Porfavor rellenar todos los campos");
+  }
+  const objResena = {
+    id: Date.now(),
+    nombre: nombrePelicula,
+    rating: ratingPelicula,
+    resena: resenaPelicula  
+  }
+  resenas = [...resenas, objResena]
+  formulario.reset();
+
+  publicarResena();
+  
+}
+
+function guardarStorage(){
+  localStorage.setItem("resenas", JSON.stringify(resenas))
+}
+window.addEventListener("DOMContentLoaded", ()=>{
+  resenas = JSON.parse(localStorage.getItem("resenas"))
+  publicarResena();
 })
 
+formulario.addEventListener("submit", agregarResena);
 
-//La barra de busqueda hace lo mismo que los botones por ahora
+
 
 
 
